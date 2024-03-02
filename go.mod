aeb3c17a0e8377ceb1b3cf30d40aac2b89b3975b
Sergey Hawr <61837079+hawr-desk@users.noreply.github.com>
2024-03-03 00:23:09 +0500

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hawr-desk/go_searchandsort.git
+
+go 1.21.6
---
+++ ssort.go
@@ -0,0 +1,9 @@
+package ssort
+
+func search_line(array []int, element int) int {
+	for i = 0; i < len(array); i++ {
+		if arr[i] == element {
+			return i
+		}
+	}
+}
